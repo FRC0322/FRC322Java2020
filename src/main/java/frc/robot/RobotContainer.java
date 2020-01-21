93716a28b20260d073d66480417cd3680b718a7e
Raa'Shaun Hunter <RaaShaun.Hunter@Gmail.com>
2020-01-21 00:38:40 -0500

    Initial Commit
    This is a basic 2020 Command-Based Robot with a 4-motor chassis.

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,63 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.DriveWithJoystick;
+import frc.robot.subsystems.Chassis;
+import frc.robot.utilities.F310Controller;
+
+/**
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final Chassis m_chassis = new Chassis();
+  
+  private final F310Controller m_driveStick = new F310Controller(0);
+
+
+  /**
+   * The container for the robot.  Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Assign default commands
+    m_chassis.setDefaultCommand(new DriveWithJoystick(
+      () -> m_driveStick.getTriggerAxis(Hand.kLeft) - m_driveStick.getTriggerAxis(Hand.kRight),
+      () -> -(m_driveStick.getX(Hand.kLeft)), m_chassis));
+
+    // Configure the button bindings
+    configureButtonBindings();
+    
+  }
+
+  /**
+   * Use this method to define your button->command mappings.  Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+  }
+
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    return null;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -0,0 +1,57 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Chassis extends SubsystemBase {
+  /**
+   * The Chassis subsystem incorporates the sensors and actuators attached to the robots chassis.
+   * It currently only includes the four drive motors.
+   */
+  private final WPI_TalonSRX m_leftFrontMotor = new WPI_TalonSRX(1);
+  private final WPI_TalonSRX m_leftRearMotor = new WPI_TalonSRX(2);
+  private final WPI_TalonSRX m_RightFrontMotor = new WPI_TalonSRX(3);
+  private final WPI_TalonSRX m_RightRearMotor = new WPI_TalonSRX(4);
+
+  private final SpeedController m_leftMotors = 
+    new SpeedControllerGroup(m_leftFrontMotor, m_leftRearMotor);
+  private final SpeedController m_rightMotors = 
+    new SpeedControllerGroup(m_RightFrontMotor, m_RightRearMotor);
+
+  private final DifferentialDrive m_drive = 
+    new DifferentialDrive(m_leftMotors, m_rightMotors);
+
+  /**
+  * Creates a new Chassis.
+  */
+  public Chassis() {
+    super();
+
+  }
+
+  /**
+  * Racing game style driving for the Chassis.
+  *
+  * @param left  Speed in range [-1,1]
+  * @param right Speed in range [-1,1]
+  */
+  public void drive(double left, double right) {
+    m_drive.arcadeDrive(left, right, true);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
