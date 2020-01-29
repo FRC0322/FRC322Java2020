4a8fad232f62d95883b99dcfdd96d934b59bcde7
Raa'Shaun Hunter <RaaShaun.Hunter@Gmail.com>
2020-01-28 23:42:05 -0500

    Minor Limelight subsystem update.

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -8,20 +8,28 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.oblarg.oblog.annotations.Log;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Limelight extends SubsystemBase {
+    /**
+   * The Limelight subsystem incorporates the Limelight 2+ camera.
+   */
   private NetworkTable m_table = NetworkTableInstance.getDefault().getTable("limelight");
   private NetworkTableEntry m_tx;
   private NetworkTableEntry m_ty;
   private NetworkTableEntry m_ta;
+  
+  @Log
+  private double x,y,area;
   /**
    * Creates a new Limelight.
    */
   public Limelight() {
+    // Create a network table instance for the limelight and variables for it's output.
     m_table = NetworkTableInstance.getDefault().getTable("limelight");
     m_tx = m_table.getEntry("tx");
     m_ty = m_table.getEntry("ty");
@@ -32,12 +40,12 @@ public class Limelight extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
     
-    //read values periodically
-    double x = m_tx.getDouble(0.0);
-    double y = m_ty.getDouble(0.0);
-    double area = m_ta.getDouble(0.0);
+    //read limelight values periodically
+    x = m_tx.getDouble(0.0);
+    y = m_ty.getDouble(0.0);
+    area = m_ta.getDouble(0.0);
 
-    //post to smart dashboard periodically
+    //post limelight values to the SmartDashboard periodically
     SmartDashboard.putNumber("LimelightX", x);
     SmartDashboard.putNumber("LimelightY", y);
     SmartDashboard.putNumber("LimelightArea", area);
