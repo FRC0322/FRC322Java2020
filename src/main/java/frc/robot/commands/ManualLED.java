fcbacbf21aa7c6938f3d4b9dd527c793a601807a
Raa'Shaun Hunter <RaaShaun.Hunter@Gmail.com>
2020-02-27 16:43:40 -0500

    Simplified the Manual LED System.

--- src/main/java/frc/robot/commands/ManualLED.java
+++ src/main/java/frc/robot/commands/ManualLED.java
@@ -7,6 +7,7 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.LED;
 
@@ -29,34 +30,19 @@ public class ManualLED extends CommandBase {
 	// Called when the command is initially scheduled.
 	@Override
 	public void initialize() {
-		try {
-			m_led.setRGB(1.0, 1.0, 1.0, 0.0);
-		}
-		catch(Exception e) {
-			System.out.println("Something LED related has failed.");
-		}
+		m_led.setRGB(Color.kWhite, 0.0);
 	}
 
 	// Called every time the scheduler runs while the command is scheduled.
 	@Override
 	public void execute() {
-		try {
-			m_led.setRGB(m_red, m_blue, m_green, m_blinkRate);
-		}
-		catch(Exception e) {
-			System.out.println("Something LED related has failed.");
-		}
+		m_led.setRGB(m_red, m_blue, m_green, m_blinkRate);
 	}
 
 	// Called once the command ends or is interrupted.
 	@Override
 	public void end(boolean interrupted) {
-		try {
-			m_led.setRGB(1.0, 1.0, 1.0, 0.0);
-		}
-		catch(Exception e) {
-			System.out.println("Something LED related has failed.");
-		}
+		m_led.setRGB(Color.kWhite, 0.0);
 	}
 
 	// Returns true when the command should end.
