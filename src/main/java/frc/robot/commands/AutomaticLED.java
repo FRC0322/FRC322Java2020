69a25d4300d4a2f69ece125b795ba23373d81f59
Raa'Shaun Hunter <RaaShaun.Hunter@Gmail.com>
2020-02-27 23:21:56 -0500

    Cleaning up imports in the AutomaticLED Command.

--- src/main/java/frc/robot/commands/AutomaticLED.java
+++ src/main/java/frc/robot/commands/AutomaticLED.java
@@ -8,7 +8,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.AddressableLEDs;
